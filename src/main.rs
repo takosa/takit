b74732a87a5f934be59e76a3e97321c028a92ba6
Takayuki Osabe <takayukiosabe@gmail.com>
2023-02-13 12:59:38 +0900

    mix bug caused by merge

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,13 @@
 use structopt::StructOpt;
 mod lib;
-<<<<<<< HEAD
-pub use crate::lib::{vcflank, randfa, randsub};
-=======
-pub use crate::lib::vcflank;
-pub use crate::lib::randfa;
-pub use crate::lib::snpdensity;
->>>>>>> 82976a2e41423a17d316748ddef8a60991863483
+pub use crate::lib::{vcflank, randfa, snpdensity, randsub};
 
 #[derive(StructOpt)]
 enum Cli {
     Vcflank(vcflank::VcflankOpt),
     Randfa(randfa::RandfaOpt),
-<<<<<<< HEAD
-    Randsub(randsub::RandsubOpt),
-=======
     Snpdensity(snpdensity::SnpdensityOpt),
->>>>>>> 82976a2e41423a17d316748ddef8a60991863483
+    Randsub(randsub::RandsubOpt),
 }
 
 fn main() {
@@ -28,13 +19,11 @@ fn main() {
         Cli::Randfa(args) => {
             randfa::randfa(args);
         },
-<<<<<<< HEAD
-        Cli::Randsub(args) => {
-            randsub::randsub(args);
-=======
         Cli::Snpdensity(args) => {
             snpdensity::snpdensity(args);
->>>>>>> 82976a2e41423a17d316748ddef8a60991863483
+        },
+        Cli::Randsub(args) => {
+            randsub::randsub(args);
         },
     }
 }
